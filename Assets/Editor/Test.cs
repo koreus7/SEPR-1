704af91cda2ca9cc35b7e075ed668c92a45d5f44
ap1278 <ap1278@york.ac.uk>
2016-01-31 17:21:31 +0000

    Added first unit test
    To check it, go to Window -> Editor Tests Runner and run it from there.
    To see the test code, go to the "Editor" folder and the script is there

---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class Test {
+
+    [Test]
+    public void EditorTest()
+    {
+        //Arrange
+        var gameObject = new GameObject();
+
+        //Act
+        //Try to rename the GameObject
+        var newGameObjectName = "My game object";
+        gameObject.name = newGameObjectName;
+
+        //Assert
+        //The object has a new name
+        Assert.AreEqual(newGameObjectName, gameObject.name);
+    }
+
+	[Test]
+	public void PlayerEnergy()
+	{
+		// Test if the playerEnergy is always between 0 and 100
+		PlayerStates state = new PlayerStates ();
+		int playerEnergy = state.energy;
+		Assert.GreaterOrEqual(playerEnergy, 0);
+		Assert.LessOrEqual(playerEnergy, 100);
+	}
+}
