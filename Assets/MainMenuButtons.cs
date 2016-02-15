8f650ae42d31257763c17e04294b65c617bd9474
Leo Mahon <leo_mahon@hotmail.com>
2016-02-15 16:50:26 +0000

    Fixed Main Menu 1
    (FIle naming issues)

---
+++ Assets/MainMenuButtons.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Link to Team Eider's website: https://eldertheduck.wordpress.com/
+/// Link to Team Shelduck's website: https://shelduck.wordpress.com/
+/// Link to Assessment 3 project version: https://eldertheduck.wordpress.com/assessment-3
+/// </summary>
+
+public class MainMenuButtons : MonoBehaviour
+{
+
+    public GameObject highScoresPanel;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void pressPlay()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void pressHighScores()
+    {
+        highScoresPanel.GetComponent<HighScoresGUI>().openPanel();
+    }
+
+}
