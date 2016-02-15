1ab3dcc85b0f5eed293c659062894681e7cd7ccd
ap1278 <ap1278@york.ac.uk>
2016-02-15 12:25:08 +0000

    Integration tests done

---
+++ Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApplyForce : MonoBehaviour {
+
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+
+		rb.AddForce (transform.forward * -200f, ForceMode.Impulse);
+
+	
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+
+	}
+}
